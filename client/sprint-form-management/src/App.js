62883cfb0249e6b9ba009ff46a3f2f0f34ce2e7b
Andrew Mendez <andrew15722@yahoo.com>
2019-08-09 12:50:49 -0700

    have the cards for the food recipes rendered, moving on to writing tests

--- client/sprint-form-management/src/App.js
+++ client/sprint-form-management/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import UserRegister from './Components/UserRegistration';
 import UserCard from './Components/UserCard';
 
+import axios from 'axios';
+
+import { Card } from 'semantic-ui-react';
+
 function App() {
+	const [ data, setData ] = useState([]);
+	useEffect(() => {
+		axios
+			.get('http://localhost:5000/api/restricted/data')
+			.then((res) => {
+				console.log(res.data);
+				setData(res.data);
+			})
+			.catch((err) => console.log(err.response));
+	}, []);
+
 	return (
 		<div className="App">
+			<Card.Group itemsPerRow={4}>
+				{data.map((food) => {
+					return <UserCard {...food} />;
+				})}
+			</Card.Group>
 			<UserRegister />
-			<UserCard />
 		</div>
 	);
 }
--- client/sprint-form-management/src/Components/UserCard.js
+++ client/sprint-form-management/src/Components/UserCard.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import axios from 'axios';
+import { Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class UserCard extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { username: '', password: '' };
-	}
-
-	async componentDidMount() {
-		const url = 'http://localhost:5000/api/restricted/data';
-		let response = await axios.get(url);
-		let data = response.data;
-		this.setState({
-			name: data.name,
-			course: data.course,
-			technique: data.technique,
-			ingredients: [ data.ingredients ]
-		});
-		console.log(data);
+		this.setState = '';
 	}
 
 	render() {
-		return <div />;
+		const extra = this.props.ingredients.map((item) => {
+			return <li>{item}</li>;
+		});
+		return (
+			<Card
+				image="https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+				header={this.props.name}
+				meta={this.props.course}
+				description={this.props.technique}
+				extra={('Ingredients', extra)}
+			/>
+		);
 	}
 }
 
